5a48f2ae005ff0f4593803d83f85f52d513ac801
navyasrimudavath07-del <navyasrimudavath07@gmail.com>
2026-04-27 20:09:10 +0530

    Add files via upload

---
+++ Queue using array.cpp
@@ -0,0 +1,79 @@
+#include<stdio.h>
+#include<stdlib.h>
+int Queue[100],n,i,front=-1,rear=-1,x,ch;
+void insert();
+void delet();
+void display();
+int main()
+{
+	//clrscr();
+	printf("enter Queue size:");
+	scanf("%d",&n);
+	printf("Queue operation:1.insert 2.delete 3.display 4.exit");
+	while(1)
+	{
+		printf("\nenter your choice");
+		scanf("%d",&ch);
+		switch(ch)
+		{
+			case1: 
+			       insert();
+			       break;
+			case2: 
+			       delet();
+			       break;  
+			case3:
+			       display();
+			       break;
+			case4: 
+			       printf("exit from Queue:");
+				   exit(0);
+			default: 
+			        printf("invalid choice");	   	        
+		}
+	}
+	return 0;
+}
+	void insert()
+	{
+		if(rear==n-1)
+		{
+			printf("Queue is full");
+		}
+		else
+		{
+			rear++;
+			printf("enter Queue element");
+			scanf("%d",&x);
+			Queue[rear]=x;
+			printf("element insereted");
+		}
+	}
+	void delet()
+	{
+		if(front==rear)
+		{
+			printf("Queue is empty");
+		}
+		else
+		{
+			front++;
+			printf("delete data element is=%d",Queue[front]);
+		}
+	}
+	void diaplay()
+	{
+		if(front==rear)
+		{
+			printf("Queue is empty");
+		}
+		else
+		{
+			printf("Queue elements are:\n");
+			for(i=0;i<=rear;i++)
+			{
+				printf("%d",Queue[i]);
+			}
+		}
+	}
+
