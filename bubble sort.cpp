9edd116a711db1f98650c4c5fca452814657dc65
navyasrimudavath07-del <navyasrimudavath07@gmail.com>
2026-04-29 21:38:05 +0530

    Add files via upload

--- bubble sort.cpp
+++ bubble sort.cpp
@@ -1,27 +1,10 @@
 #include<stdio.h>
 int main()
 {
-	int i,n,j,a[n],temp;
-	printf("enter size of array");
-	scanf("%d",&n);
-	printf("enter array value");
-	for(i=0;i<n;i++)
-	{
-		scanf("%d",&a[i]);
-	}
-		for(j=0;j<n-i-1;j++)
-		{
-			if(a[j]>a[j+1])
-			{
-				temp=a[j];
-				a[j]=a[j+1];
-				a[j+1]=temp;
-			}
-		}
-	printf("\n sorted array is :");
-	for(i=0;i<n;i++) 
-	{
-		printf("%d",a[i]);
-	}
+	int x=12;
+	int *ptr=NULL;
+	if (ptr==NULL)
+	ptr=&x;
+	printf("value of ptr =%d",*ptr);
 	return 0;
 }
